420ae78485bc9888142423466533cd7ec5b5ebb4
Solya <catsubrosa@gmail.com>
2024-10-22 15:29:38 +0300

    task/350497:Add subtheme and template

--- web/modules/custom/solych/src/Controller/CatsPageController.php
+++ web/modules/custom/solych/src/Controller/CatsPageController.php
@@ -6,10 +6,9 @@ use Drupal\Core\Controller\ControllerBase;
 class CatsPageController extends ControllerBase {
   public function content() {
     return [
-      '#theme' => 'page',
-      '#type' => 'markup',
-      '#title' => $this->t('Cats page'),
-      '#markup' => $this->t('Hello! You can add here a photo of your cat.'),
+      '#theme' => 'cats-page',
+      '#title' => 'Cats page',
+      '#markup' =>'Hello! You can add here a photo of your cat.',
     ];
   }
 }
