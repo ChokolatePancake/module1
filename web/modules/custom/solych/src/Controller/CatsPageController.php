47d68661f5bd71043e006bcb2692055b1f8f3816
Solya <catsubrosa@gmail.com>
2024-10-22 11:57:33 +0300

    Move template to module

--- web/modules/custom/solych/src/Controller/CatsPageController.php
+++ web/modules/custom/solych/src/Controller/CatsPageController.php
@@ -6,7 +6,9 @@ use Drupal\Core\Controller\ControllerBase;
 class CatsPageController extends ControllerBase {
   public function content() {
     return [
+      '#theme' => 'page',
       '#type' => 'markup',
+      '#title' => $this->t('Cats page'),
       '#markup' => $this->t('Hello! You can add here a photo of your cat.'),
     ];
   }
