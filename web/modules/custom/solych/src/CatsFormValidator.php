257c66f141c87934de315d444c83b295e74eb7f0
Solya <catsubrosa@gmail.com>
2024-10-24 15:40:06 +0300

    task/350506: Added field email and real-time validation for email and cat's name

--- web/modules/custom/solych/src/CatsFormValidator.php
+++ web/modules/custom/solych/src/CatsFormValidator.php
@@ -2,23 +2,86 @@
 
 namespace Drupal\solych;
 
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\InvokeCommand;
 use Drupal\Core\Form\FormStateInterface;
 
 class CatsFormValidator {
 
   /**
-   * Validates the Cats form submission.
+   * Validates the email address.
    *
-   * @param array $form
-   *   The form array.
+   * @param string $email
+   *   The email address to validate.
    * @param \Drupal\Core\Form\FormStateInterface $form_state
    *   The current state of the form.
+   *
+   * @return string|null
+   *   An error message if the validation fails, or NULL if the email is valid.
    */
-  public function validateForm($form_id, array $form, FormStateInterface $form_state,) {
-    $cat_name = $form_state->getValue('cat_name');
+  public function validateEmail($email, FormStateInterface $form_state) {
+    $parts = explode('@', $email);
 
-    if (mb_strlen($cat_name) < 2 || mb_strlen($cat_name) > 32) {
-      $form_state->setErrorByName('cat_name', t('The cat name must be between 2 and 32 characters.'));
+    if (count($parts) != 2) {
+      return t('The email address must contain exactly one "@" symbol.');
     }
+
+    $first_part = $parts[0];
+    $first_pattern = '/^[a-zA-Z0-9_\-.]+$/';
+
+    if (!preg_match($first_pattern, $first_part)) {
+      return t('The first part of the email (before the "@") can only contain Latin letters, numbers, underscores (_), hyphens (-), and dots (.).');
+    }
+
+    $domain_part = '@' . $parts[1];
+    $domain_pattern = '/@[a-zA-Z0-9\-]+\.[a-zA-Z]{2,}$/';
+
+    if (!preg_match($domain_pattern, $domain_part)) {
+      return t('The domain part of the email (after the "@") is not valid. It should be in the format "example.com".');
+    }
+    return NULL;
+  }
+
+/**
+ * Validates the cat's name.
+ *
+ * @param string $name
+ *   The name to validate.
+ * @param \Drupal\Core\Form\FormStateInterface $form_state
+ *   The current state of the form.
+ *
+ * @return string|null
+ *   An error message if the validation fails, or NULL if the email is valid.
+ */
+  public function validateName(string $name, FormStateInterface $form_state) {
+    if (mb_strlen($name) < 2 || mb_strlen($name) > 32) {
+      return t('The cat name must be between 2 and 32 characters.');
+    }
+    return NULL;
+  }
+
+  /**
+   * Reusable method for handling AJAX validation.
+   *
+   * @param string $selector
+   *   The jQuery selector for the element to update.
+   * @param string $message
+   *   The validation message.
+   *
+   * @return \Drupal\Core\Ajax\AjaxResponse
+   *   The AJAX response with commands.
+   */
+  public function handleValidationAjax($selector, $message) {
+    $response = new AjaxResponse();
+    if ($message) {
+      $response->addCommand(new InvokeCommand($selector, 'addClass', ['hint']));
+    }
+    else {
+      $response->addCommand(new InvokeCommand($selector, 'removeClass', ['hint']));
+    }
+
+    $response->addCommand(new InvokeCommand($selector, 'html', [$message]));
+
+    return $response;
   }
 }
--- web/modules/custom/solych/src/Form/CatsForm.php
+++ web/modules/custom/solych/src/Form/CatsForm.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\solych\Form;
 
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\InvokeCommand;
 use Drupal\solych\CatsFormValidator;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Form\FormBase;
@@ -19,6 +21,20 @@ class CatsForm extends FormBase {
 
 
   /**
+   * The custom form validator.
+   *
+   * @var \Drupal\solych\CatsFormValidator
+   */
+  protected $validator;
+
+  /**
+   * The AjaxResponse object for handling AJAX commands.
+   *
+   * @var \Drupal\Core\Ajax\AjaxResponse
+   */
+  protected $response;
+
+  /**
    * Constructor for injection Messenger service.
    *
    * @param \Drupal\Core\Messenger\MessengerInterface $messenger
@@ -26,6 +42,8 @@ class CatsForm extends FormBase {
    */
   public function __construct(MessengerInterface $messenger) {
     $this->messenger = $messenger;
+    $this->validator = new CatsFormValidator();
+    $response = new AjaxResponse();
   }
 
   /**
@@ -52,7 +70,35 @@ class CatsForm extends FormBase {
       '#title' => $this->t('Your cat’s name:'),
       '#required' => TRUE,
       '#description' => $this->t('Minimal length of name:2 characters. Maximal length of name:32 characters.'),
+      '#ajax' => [
+        'callback' => '::validateNameAjax',
+        'event' => 'change',
+        'wrapper' => 'name-validation-message',
+      ],
     ];
+
+    $form['name_validation_message'] = [
+      '#type' => 'container',
+      '#attributes' => ['id' => 'name-validation-message'],
+    ];
+
+    $form['email'] = [
+      '#type' => 'email',
+      '#title' => $this->t('Your Email:'),
+      '#required' => TRUE,
+      '#ajax' => [
+        'callback' => '::validateEmailAjax',
+        'event' => 'change',
+        'wrapper' => 'email-validation-message',
+      ],
+      '#description' => $this->t('Please enter a valid email(only latin letters, numbers, underscores or hyphens).'),
+    ];
+
+    $form['email_validation_message'] = [
+      '#type' => 'container',
+      '#attributes' => ['id' => 'email-validation-message'],
+    ];
+
 
     $form['actions']['submit'] = [
       '#type' => 'submit',
@@ -61,7 +107,8 @@ class CatsForm extends FormBase {
         'callback' => '::ajaxSubmit',
         'wrapper' => 'cats-form-messages',
         'effect' => 'fade',
-      ]
+      ],
+      '#attributes' => ['class' => ['submit-cats-form']]
     ];
 
     $form['#prefix'] = '<div id="cats-form-messages">';
@@ -72,12 +119,60 @@ class CatsForm extends FormBase {
   }
 
   /**
+   * AJAX callback for real-time email validation.
+   *
+   * @param array &$form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   *
+   * @return \Drupal\Core\Ajax\AjaxResponse
+   *   A response object to manipulate the page.
+   */
+  public function validateEmailAjax(array &$form, FormStateInterface $form_state) {
+    $email = $form_state->getValue('email');
+    $error_message = $this->validator->validateEmail($email, $form_state);
+
+    return $this->validator->handleValidationAjax('#email-validation-message', $error_message);
+  }
+
+  /**
+   * AJAX callback for real-time name validation.
+   *
+   * @param array &$form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   *
+   * @return \Drupal\Core\Ajax\AjaxResponse
+   *   A response object to manipulate the page.
+   */
+  public function validateNameAjax(array &$form, FormStateInterface $form_state) {
+    $name = $form_state->getValue('cat_name');
+    $error_message = $this->validator->validateName($name, $form_state);
+
+    return $this->validator->handleValidationAjax('#name-validation-message', $error_message);
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    // Create an instance of the validator and validate the form.
-    $validator = new CatsFormValidator();
-    $validator->validateForm($this->getFormId(), $form, $form_state);
+    $name = $form_state->getValue('cat_name');
+
+    $email = $form_state->getValue('email');
+
+    $error_message = $this->validator->validateName($name, $form_state);
+
+    if ($error_message) {
+      $form_state->setErrorByName('name', $error_message);
+    }
+
+    $error_message = $this->validator->validateEmail($email, $form_state);
+
+    if ($error_message) {
+      $form_state->setErrorByName('email', $error_message);
+    }
   }
 
 
@@ -115,6 +210,7 @@ class CatsForm extends FormBase {
 
     $form_state->setRebuild(TRUE);
     $form['cat_name']['#value'] = '';
+    $form['email']['#value'] = '';
     return $form;
   }
 
